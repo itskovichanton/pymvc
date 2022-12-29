c703b443655b64450fb9f4b83625bfab1620b180
aitskovich <a.itskovich@molbulak.com>
2022-12-30 01:27:35 +0300

    exceptions.py fixed

--- src/mybootstrap_mvc_itskovichanton/exceptions.py
+++ src/mybootstrap_mvc_itskovichanton/exceptions.py
@@ -1,3 +1,5 @@
+import functools
+
 ERR_REASON_SERVER_RESPONDED_WITH_ERROR = "ERRCODE_SERVER_RESPONDED_WITH_ERROR"
 ERR_REASON_VALIDATION = "VALIDATION"
 ERR_REASON_AUTH_REQUIRED = "AUTHORIZATION_REQUIRED"
@@ -13,10 +15,30 @@ ERR_REASON_TOO_MANY_REQUESTS = "TO_MANY_REQUESTS"
 class CoreException(BaseException):
     """Base exception, that has message and reason properties"""
 
-    def __init__(self, message = None, reason=ERR_REASON_SERVER_RESPONDED_WITH_ERROR, cause: BaseException = None):
+    def __init__(self, message=None, reason=ERR_REASON_SERVER_RESPONDED_WITH_ERROR, cause: BaseException = None):
         if not message and cause:
             message = str(cause)
         self.reason = reason
         self.message = message
         self.cause = cause
         super().__init__(self.message)
+
+
+def wrap_with_core_exception(_reason=ERR_REASON_SERVER_RESPONDED_WITH_ERROR, _message=None):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except BaseException as e:
+                message = _message
+                if callable(message):
+                    message = message(e)
+                reason = _reason
+                if callable(reason):
+                    reason = reason(e)
+                raise CoreException(reason=reason, cause=e, message=message)
+
+        return wrapper
+
+    return decorator_repeat
