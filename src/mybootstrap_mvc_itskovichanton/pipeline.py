3e3e50ce84a9baa125183357fdb56009f6f34c4d
aitskovich <a.itskovich@molbulak.com>
2023-05-09 00:41:28 +0300

    prev_result - удалил

--- src/mybootstrap_mvc_itskovichanton/pipeline.py
+++ src/mybootstrap_mvc_itskovichanton/pipeline.py
@@ -4,7 +4,6 @@ from typing import Any, Callable
 
 from src.mybootstrap_core_itskovichanton.alerts import AlertService
 from src.mybootstrap_ioc_itskovichanton.ioc import bean
-
 from src.mybootstrap_mvc_itskovichanton.error_provider import Err, ErrorProvider
 
 
@@ -22,23 +21,23 @@ class Result:
 
 
 class Action:
-    def run(self, args: Any = None, prev_result: Any = None) -> Any:
+    def run(self, args: Any = None) -> Any:
         """Run action"""
 
 
 @dataclass
 class CallableAction(Action):
-    call: Callable[[Any, Any], Any]
+    call: Callable[[Any], Any]
 
-    def run(self, args: Any = None, prev_result: Any = None) -> Any:
-        return self.call(args, prev_result)
+    def run(self, args: Any = None) -> Any:
+        return self.call(args)
 
 
 class ActionRunner:
 
-    async def run(self, *actions: Action | Callable[[Any, Any], Any], call: Any = None,
+    async def run(self, *actions: Action | Callable[[Any], Any], call: Any = None,
                   error_provider: ErrorProvider = None) -> Result:
-        """runs action"""
+        ...
 
 
 @bean
@@ -46,22 +45,27 @@ class ActionRunnerImpl(ActionRunner):
     default_error_provider: ErrorProvider
     alert_service: AlertService
 
-    async def run(self, *actions: Action | Callable[[Any, Any], Any], call: Any = None,
+    async def run(self,
+                  *actions: Action | Callable[[Any], Any],
+                  call: Any = None,
                   error_provider: ErrorProvider = None) -> Result:
+
         r = Result()
-        for action in actions[0]:
+        r.result = call
+        for action in actions:
             if callable(action) and not isinstance(action, Action):
                 action = CallableAction(call=action)
             try:
                 if inspect.iscoroutinefunction(action.run):
-                    r.result = await action.run(call, r.result)
+                    r.result = await action.run(r.result)
                 else:
-                    r.result = action.run(call, r.result)
+                    r.result = action.run(r.result)
             except BaseException as e:
                 self.alert_service.handle(e)
                 if error_provider is None:
                     error_provider = self.default_error_provider
                 r.error = error_provider.provide_error(e)
+                r.result = None
                 break
 
         return r
